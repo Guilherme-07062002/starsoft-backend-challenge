9a52b063209a1c23634c441e2b747278f7b0715d
Guilherme-07062002 <guilherme762002@gmail.com>
2026-01-20 15:33:09 -0300

    feat: adicionar configuração do Swagger para documentação da API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+    .setTitle('Starsoft Backend Challenge')
+    .setDescription('API para gerenciamento de sessões de cinema e assentos.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule
+    .createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory());
   await app.listen(3000);
 }
 bootstrap();
