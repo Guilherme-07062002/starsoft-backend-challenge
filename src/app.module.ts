8550f0649e1ac023a11bd418c7b94f28ae34ceef
Guilherme-07062002 <guilherme762002@gmail.com>
2026-01-20 16:59:26 -0300

    feat: adicionar configuração de rate limiting com Throttler e Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -8,9 +9,26 @@ import { RedisModule } from './redis/redis.module';
 import { MessagingModule } from './rabbitmq/rabbitmq.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { NotificationsModule } from './notifications/notifications.module';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 
 @Module({
   imports: [
+    // Configuração do Rate Limiting, está configurado para 10 requisições por minuto por IP
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: seconds(60), // 60 segundos
+          limit: 10,  // 10 requisições
+        },
+      ],
+      storage: new ThrottlerStorageRedisService({
+        host: process.env.REDIS_HOST || 'redis',
+        port: parseInt(process.env.REDIS_PORT || '6379'),
+        // password: process.env.REDIS_PASSWORD se houver senha
+      }),
+    }),
+
     // Infra
     ScheduleModule.forRoot(), // O forRoot inicializa o módulo de agendamento
     PrismaModule,
@@ -23,6 +41,12 @@ import { NotificationsModule } from './notifications/notifications.module';
     NotificationsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
