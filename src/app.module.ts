121f63d377ba8466f5f8a7981189ae94f631787c
Guilherme-07062002 <guilherme762002@gmail.com>
2026-01-21 22:30:54 -0300

    refactor: remover AppController, AppService e DTOs de notificação não utilizados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { ReservationsModule } from './reservations/reservations.module';
@@ -61,9 +59,7 @@ import { SalesModule } from './sales/sales.module';
     NotificationsModule,
     SalesModule,
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: AppThrottlerGuard, // Usa o guard customizado para Rate Limiting
